acbe1b82b34719095a8b803345c08109b5c23111
digonad4 <rpirezinho@gmail.com>
2025-03-12 07:10:00 -0300

    refatora estilos do gráfico e simplifica a exportação do componente Toggle

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -206,21 +206,17 @@ const ChartTooltipContent = React.forwardRef<
                       !hideIndicator && (
                         <div
                           className={cn(
-                            "shrink-0 rounded-[2px] border-[--color-border] bg-[--color-bg]",
+                            "shrink-0 rounded-[2px] border-indicator bg-indicator",
                             {
                               "h-2.5 w-2.5": indicator === "dot",
                               "w-1": indicator === "line",
                               "w-0 border-[1.5px] border-dashed bg-transparent":
                                 indicator === "dashed",
                               "my-0.5": nestLabel && indicator === "dashed",
-                            }
+                            },
+                            "indicator-style"
                           )}
-                          style={
-                            {
-                              "--color-bg": indicatorColor,
-                              "--color-border": indicatorColor,
-                            } as React.CSSProperties
-                          }
+                          data-indicator-color={indicatorColor}
                         />
                       )
                     )}
@@ -298,10 +294,9 @@ const ChartLegendContent = React.forwardRef<
                 <itemConfig.icon />
               ) : (
                 <div
-                  className="h-2 w-2 shrink-0 rounded-[2px]"
-                  style={{
-                    backgroundColor: item.color,
-                  }}
+                  className={cn("indicator", {
+                    "bg-[item.color]": item.color,
+                  })}
                 />
               )}
               {itemConfig?.label}
