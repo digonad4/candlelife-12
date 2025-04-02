71126cc4f2a170cc89d20238d60c77f08a52c39e
digonad4 <rpirezinho@gmail.com>
2025-04-02 02:51:42 -0300

    feat: Adicionar configuração do Capacitor e dependências necessárias

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.candlelife.app',
+  appName: 'CandleLife',
+  webDir: 'dist'
+};
+
+export default config;
