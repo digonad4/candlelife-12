298f2573ac17185eedd0928f161a7b6453946701
digonad4 <rpirezinho@gmail.com>
2025-04-02 02:56:57 -0300

    fix: Atualizar diretório webDir para 'www' na configuração do Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.candlelife.app',
   appName: 'CandleLife',
-  webDir: 'dist'
+  webDir: 'www'
 };
 
 export default config;
