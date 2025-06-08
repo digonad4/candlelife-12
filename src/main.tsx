3db5d1123dc4d56a42ebb1afe359d8e7f99582b8
RODRIGO PIRES DA SILVA <46466752+digonad4@users.noreply.github.com>
2025-06-08 05:06:08 -0300

    olookooo

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,9 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-<<<<<<< HEAD
   <SidebarProvider defaultOpen={false}>
     <div className="min-h-screen flex w-full">
       <App />
     </div>
   </SidebarProvider>
-=======
-  <React.StrictMode>
-    <SidebarProvider defaultOpen={false}>
-      <div className="min-h-screen flex w-full">
-        <App />
-      </div>
-    </SidebarProvider>
-  </React.StrictMode>
->>>>>>> 3797327d60e6ce08ca10aa8ec6ff27311f492b43
 );
