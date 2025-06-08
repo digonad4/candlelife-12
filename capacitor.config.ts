48aaa5907cac6b3dd978fa9a623b678198965700
digonad4 <rpirezinho@gmail.com>
2025-06-08 14:55:05 -0300

    Que loucura

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.candlelife.app',
-  appName: 'Candle_Life',
+  appName: 'candle-life',
   webDir: 'dist'
 };
 
